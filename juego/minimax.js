84dbb2d7ad510aac67569152a7086470e3e75e2f
Atter12xd <1485275@senati.pe>
2024-10-24 00:44:08 -0500

    Generación de Movimientos Posibles

--- juego/minimax.js
+++ juego/minimax.js
@@ -3,11 +3,24 @@ function minimax(newBoard, player) {
         if (curr === '') acc.push(index);
         return acc;
     }, []);
-    
+
     if (checkWin(newBoard, 'O')) {
         return { score: -10 };
     } else if (checkWin(newBoard, 'X')) {
         return { score: 10 };
     } else if (availableSpots.length === 0) {
         return { score: 0 };
+    }
+
+    const moves = [];
+    for (let i = 0; i < availableSpots.length; i++) {
+        const move = {};
+        move.index = availableSpots[i];
+        newBoard[move.index] = player;
+
+        const result = minimax(newBoard, player === 'X' ? 'O' : 'X');
+        move.score = result.score;
+
+        newBoard[move.index] = ''; // Reset the spot
+        moves.push(move);
     }
